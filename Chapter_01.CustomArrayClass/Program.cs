a8328e27c0cf3ed7488e5d79244a80411e61384a
Erdinç Dursun <erdincdursun20@gmail.com>
2023-11-23 09:17:00 +0300

    Chapter_01_Started. Custom Array Written

---
+++ Chapter_01.CustomArrayClass/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Chapter_01.CustomArrayClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray myArray = new MyArray();
+
+            myArray.Push("Hello");
+            myArray.Push("BusBus");
+            myArray.Push("!");
+            myArray.Pop();
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                Console.WriteLine(myArray.Get(i));
+            }
+
+        }
+    }
+}
