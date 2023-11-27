cc904309f7d08083df6100c21205fef268e5bf04
Erdinç Dursun <erdincdursun20@gmail.com>
2023-11-27 08:32:08 +0300

    I started Hastable

---
+++ Chapter_02.HashTables/MyNode.cs
@@ -0,0 +1,14 @@
+﻿namespace Chapter_02.HashTables
+{
+	class MyNode
+	{
+		public string key { get; set; }
+		public int value { get; set; }
+		public MyNode(string key, int value)
+		{
+			this.key = key;
+			this.value = value;
+		}
+
+	}
+}
---
+++ Chapter_02.HashTables/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Chapter_02.HashTables
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+		}
+	}
+
+	class HashTable
+	{
+		private class MyNodes : List<MyNode> { }
+		private int length;
+		private MyNodes[] data;
+
+		public HashTable(int size)
+		{
+			this.length = size;
+			this.data = new MyNodes[size];	
+		}
+
+		private int Hash(string key)
+		{
+			int hash = 0;
+            for (int i = 0; i < key.Length; i++)
+            {
+				hash = (hash + (int)key[i] * i) % this.length; 
+            }
+			return hash;
+        }
+		public void Set(string key, int value)
+		{
+			int index = Hash(key);
+            if (this.data[index] == null)
+            {
+				this.data[index] = new MyNodes();
+            }
+			this.data[index].Add(new MyNode(key,value));
+        }
+	}
+}
