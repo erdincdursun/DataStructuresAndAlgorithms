5b99eda660f5be379d56d11daff65d8bfefa1949
Erdinç Dursun <busradursunn20@gmail.com>
2023-12-14 07:52:14 +0300

    Started-Chapter.05. Topic  : Trees

---
+++ Chapter_05.Trees/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Chapter_05.Trees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
